da04668594e830f9859fde291389895426f9b9c4
Matt Jibson <matt.jibson@gmail.com>
2018-06-13 20:20:23 -0400

    make a real test

--- pretty/prettier_test.go
+++ pretty/prettier_test.go
@@ -1,19 +1,22 @@
-package pretty
+package pretty_test
 
 import (
 	"fmt"
 	"testing"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/parser"
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
+	"github.com/mjibson/sqlfmt/pretty"
 )
 
 func TestPrettier(t *testing.T) {
-	d := Stack()
-	last := ""
-	for i := 0; i < 50; i++ {
-		s, _ := PrettyString(d, i)
-		if s == last {
-			continue
-		}
-		last = s
-		fmt.Printf("%d:\n%s\n\n", i, s)
+	const sql = `SELECT count(*) count, winner, counter * 60 * 5 as counter FROM (SELECT winner, round(length / 60 / 5) as counter FROM players WHERE build = $1 AND (hero = $2 OR region = $3)) GROUP BY winner, counter`
+
+	stmt, err := parser.ParseOne(sql)
+	if err != nil {
+		t.Fatal(err)
 	}
+	got, err := pretty.PrettyString(tree.Doc(stmt), 20)
+
+	fmt.Println(got)
 }
