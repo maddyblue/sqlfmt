e17b883646a413918c93191071307b8b7977b6fb
Matt Jibson <matt.jibson@gmail.com>
2018-06-18 19:09:16 -0400

    share link
    Fixes #3

--- main.go
+++ main.go
@@ -149,7 +149,7 @@ input {
 <textarea id="sql" style="width: 100%; height: 150px" onChange="range()" onInput="range()">SELECT count(*) count, winner, counter * 60 * 5 as counter FROM (SELECT winner, round(length / 60 / 5) as counter FROM players WHERE build = $1 AND (hero = $2 OR region = $3)) GROUP BY winner, counter</textarea>
 <br><input type="range" min="1" max="200" step="1" name="n" value="40" onChange="range()" onInput="range()" id="n" style="width: 100%">
 <br>target width: <span id="nval"></span>, actual width: <span id="actual_width"></span>
-<br><button id="copy">copy to clipboard</button>
+<br><button id="copy">copy to clipboard</button> <a href="" id="share">share</a>
 <br><pre id="fmt"></pre>
 <pre id="width" style="position: absolute; visibility: hidden; height: auto; width: auto;">_</pre>
 <hr>
@@ -161,6 +161,7 @@ const actual = document.getElementById('actual_width');
 const n = document.getElementById('n');
 const fmt = document.getElementById('fmt');
 const sqlEl = document.getElementById('sql');
+const share = document.getElementById('share');
 
 document.getElementById('copy').addEventListener('click', ev => {
 	copyTextToClipboard(fmt.innerText);
@@ -233,9 +234,11 @@ function range() {
 				if (data.length === 1 && data[0].includes('syntax error')) {
 					fmt.innerText = data[0];
 					actual.innerText = '';
+					share.href = '#';
 				} else {
 					fmt.innerText = data.map(d => d + ';').join('\n\n');
 					actual.innerText = Math.max(...fmt.innerText.split('\n').map(v => v.length));
+					share.href = '/?n=' + v + '&sql=' + encodeURIComponent(b64EncodeUnicode(sql));
 				}
 				if (pending) {
 					range();
@@ -247,6 +250,32 @@ function range() {
 			working = false;
 			console.log(d);
 		}
+	);
+}
+
+function b64EncodeUnicode(str) {
+	// first we use encodeURIComponent to get percent-encoded UTF-8,
+	// then we convert the percent encodings into raw bytes which
+	// can be fed into btoa.
+	return btoa(
+		encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function toSolidBytes(
+			match,
+			p1
+		) {
+			return String.fromCharCode('0x' + p1);
+		})
+	);
+}
+
+function b64DecodeUnicode(str) {
+	// Going backwards: from bytestream, to percent-encoding, to original string.
+	return decodeURIComponent(
+		atob(str)
+			.split('')
+			.map(function(c) {
+				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+			})
+			.join('')
 	);
 }
 
@@ -266,8 +295,13 @@ document.addEventListener('keydown', e => {
 });
 
 (() => {
-	const sql = localStorage.getItem('sql');
-	const nVal = localStorage.getItem('n');
+	const search = new URLSearchParams(location.search);
+	if (location.search) {
+		window.history.replaceState(null, '', '/');
+	}
+	let sql = search.get('sql');
+	sql = sql ? b64DecodeUnicode(sql) : localStorage.getItem('sql');
+	const nVal = search.get('n') || localStorage.getItem('n');
 	if (sql !== null && sql != '') {
 		sqlEl.innerText = sql;
 	}
