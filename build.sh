74d6450d0a37f0aa2477e6d4db881375d2f78f82
Matt Jibson <matt.jibson@gmail.com>
2019-07-03 14:03:04 -0400

    update build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -ex
 go build -o sqlfmt
 BRANCH=$(git symbolic-ref --short HEAD)
 SHA=$(git rev-parse --short HEAD)
-gcloud --project cockroach-dev-inf builds submit --substitutions=BRANCH_NAME=$BRANCH,SHORT_SHA=$SHA
+gcloud --project cockroach-dev-inf builds submit --substitutions=BRANCH_NAME=$BRANCH,SHORT_SHA=$SHA --config cloudbuild.yaml
 
 # kubectl get po | grep sqlfmt | awk '{print $1}' | xargs kubectl delete po
